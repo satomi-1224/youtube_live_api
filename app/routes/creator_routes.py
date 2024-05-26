457a8a50805ff17eec0981c2dbce2744ac2e2603
OkadaKazuhiro0228 <okadakazuhiro0228@gmail.com>
2024-05-26 18:27:40 +0900

    クリエーターをchannelId検索で一件取得のAPI作成

--- app/routes/creator_routes.py
+++ app/routes/creator_routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends
 from typing import List
+from typing import Optional
 from config.app import base_dir
 from services.creator.creator_service import CreatorService
 
 from schemas.creator.responses.items_response import ItemsResponse
 from schemas.creator.requests.items_request import ItemsRequest
+from schemas.creator.requests.item_request import ItemRequest
 
 from utils.filters.creator.items_filter import ItemsFilter
 
@@ -25,5 +27,12 @@ async def get_items(request: ItemsRequest = Depends(ItemsRequest)) -> List[Items
     return filter_creators
 
 @router.get('/item')
-async def get_items():
-    return {'items': ['item1', 'port']}
+async def get_item(request: ItemRequest = Depends(ItemRequest)) -> Optional[ItemsResponse]:
+    creator_dir = f'{base_dir}/creators'
+    files = await aiofiles.os.listdir(creator_dir)
+    file_paths = [f'{creator_dir}/{fp}' for fp in files]
+    
+    creators = await service.get_items(file_paths)
+    filter_creator = ItemsFilter.filterBychannelId(request, creators)
+    
+    return filter_creator
--- app/utils/filters/creator/items_filter.py
+++ app/utils/filters/creator/items_filter.py
@@ -1,6 +1,9 @@
 from typing import List
+from typing import Optional
 from schemas.creator.responses.items_response import ItemsResponse
 from schemas.creator.requests.items_request import ItemsRequest
+from schemas.creator.requests.item_request import ItemRequest
+
 
 class ItemsFilter():
     @staticmethod
@@ -9,3 +12,11 @@ class ItemsFilter():
             creators = [creator for creator in creators if creator['channelTag'] == request.channelTag]
             
         return creators
+    
+    def filterBychannelId(request: ItemRequest, creators: List[ItemsResponse]) -> Optional[ItemsResponse]:
+        if request.channelId != None:
+            for creator in creators:
+                if creator['channelId'] == request.channelId:
+                    return creator
+                            
+        return None
